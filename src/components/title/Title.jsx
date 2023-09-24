0f59b053a47d0ca187d670a8e6e791469eba28b5
pRemant <paudelremant45@gmail.com>
2023-09-24 21:59:11 +0545

    title updated

--- src/components/title/Title.jsx
+++ src/components/title/Title.jsx
@@ -5,7 +5,7 @@ const Title = ({ titleProps }) => {
   return (
     <>
       <div className='title text-md-center'>
-        <h2>{titleProps}</h2>
+        <h2 className='pb-1 pb-md-3'>{titleProps}</h2>
       </div>
     </>
   )
