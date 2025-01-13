87bb1af72ed875f87645b3f046b8ebdb37af3103
remant <paudelremant45@gmail.com>
2025-01-13 08:37:30 +0545

    add layout file

---
+++ src/components/common/Layout.jsx
@@ -0,0 +1,16 @@
+// Layout.js
+import React from 'react';
+import Footer from './Footer';
+import NavBar from './NavBar';
+
+const Layout = ({ children }) => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <NavBar />
+      <main className="flex-grow">{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
